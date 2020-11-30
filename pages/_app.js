4fdf9f9a28289191159a6c867df209c4617796b2
Onur <onur.haymana@kariyer.net>
2020-11-30 21:30:34 +0300

    added express

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,8 @@
 import '../styles/globals.css'
 import '../styles/todo.css'
 
+app.use(express.static(path.join(__dirname, 'public/index.html')));
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
