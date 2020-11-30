e6497a088e4a14c3ae54f1e5fc78b92797282708
Onur <onur.haymana@kariyer.net>
2020-11-30 22:19:34 +0300

    fixed 500

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     env: {
-        MONGO_URI: "mongodb+srv://onur:onur1234@cluster0.zaoao.mongodb.net/<dbname>?retryWrites=true&w=majority"
+        NEXT_PUBLIC_MONGO_URI: "mongodb+srv://onur:onur1234@cluster0.zaoao.mongodb.net/<dbname>?retryWrites=true&w=majority"
     }
 }
