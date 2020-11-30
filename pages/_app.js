331470f186749ff220b44b0454830b3caf46ca27
Onur <onur.haymana@kariyer.net>
2020-11-30 21:51:55 +0300

    continue

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,6 @@
 import '../styles/globals.css'
 import '../styles/todo.css'
 
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
