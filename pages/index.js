a12faca83a65f66d6eca6f65ef0ddf5cd0b54334
Onur <onur.haymana@kariyer.net>
2020-12-01 03:05:06 +0300

    Added Deadline Date Opt

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'
+import Moment from 'react-moment'
 import Head from 'next/head'
 
 function Task({ task, index, completeTask, removeTask }) {
@@ -11,18 +12,23 @@ function Task({ task, index, completeTask, removeTask }) {
 
             <button style={{ background: "red" }} onClick={() => removeTask(index, task._id)}>x</button>
             <button onClick={() => completeTask(index, task._id)}>{task.completed ? "Completed" : "Uncompleted"}</button>
+           {(task.date) && ( <button><Moment fromNow later>{task.date}</Moment></button> )}
         </div>
-    );
+    )
 }
 
 function GenerateTask({ createTask }) {
-    const [value, setValue] = useState("");
+    const [value, setValue] = useState("")
+    const [date, setDate] = useState("")
 
     const handleSubmit = e => {
-        e.preventDefault();
-        if (!value) return;
-        createTask(value);
-        setValue("");
+      e.preventDefault()
+        setDate(date)
+
+        if (!value) return
+        createTask(value, date)
+        setValue("")
+
     }
     return (
         <form onSubmit={handleSubmit}>
@@ -30,23 +36,30 @@ function GenerateTask({ createTask }) {
                 type="text"
                 className="input"
                 value={value}
-                placeholder="Add a new task"
+                placeholder="Add a new task, deadline date optional"
                 onChange={e => setValue(e.target.value)}
+            />
+              <input
+                type="datetime-local"
+                className="input"
+                date={date}
+                onChange={d => setDate(d.target.value)}
             />
                 <button onClick={handleSubmit}>Create</button>
         </form>
-    );
+    )
 }
 
 function Todo({ notes }) {
-    const [tasksRemaining, setTasksRemaining] = useState(0);
-    const [tasks, setTasks] = useState(notes);
+    const [tasksRemaining, setTasksRemaining] = useState(0)
+    const [tasks, setTasks] = useState(notes)
 
-    useEffect(() => { setTasksRemaining(tasks.filter(task => !task.completed).length) });
+    useEffect(() => { setTasksRemaining(tasks.filter(task => !task.completed).length) })
 
-    const createTask = async title => {
-        const newTasks = [...tasks, { title, completed: false }];
-        setTasks(newTasks);
+    const createTask = async (title, date) => {
+        console.log(title, date)
+        const newTasks = [...tasks, { title, completed: false, date }]
+        setTasks(newTasks)
 
         try {
             const added = await fetch(`${process.env.NEXT_PUBLIC_API_URI}/api/todos`, {
@@ -55,22 +68,22 @@ function Todo({ notes }) {
                     "Accept": "application/json",
                     "Content-Type": "application/json"
                 },
-                body: JSON.stringify({title, completed:false, date:new Date()})
+                body: JSON.stringify({title, completed:false, date})
              
             })
            
         } catch (error) {
-            console.log(error);
+            console.log(error)
         }
-    };
+    }
 
     const completeTask = async (index, id) => {
     
-        const newTasks = [...tasks];
+        const newTasks = [...tasks]
 
         if (newTasks[index].completed) {
-            newTasks[index].completed = false;
-            setTasks(newTasks);
+            newTasks[index].completed = false
+            setTasks(newTasks)
 
             try {
                 const uncomplated = await fetch(`${process.env.NEXT_PUBLIC_API_URI}/api/todos/${id}`, {
@@ -83,12 +96,12 @@ function Todo({ notes }) {
                  
                 })
             } catch (error) {
-                console.log(error);
+                console.log(error)
             }
         }
         else {
-            newTasks[index].completed = true;
-            setTasks(newTasks);
+            newTasks[index].completed = true
+            setTasks(newTasks)
 
             try {
                 const uncomplated = await fetch(`${process.env.NEXT_PUBLIC_API_URI}/api/todos/${id}`, {
@@ -101,25 +114,25 @@ function Todo({ notes }) {
                  
                 })
             } catch (error) {
-                console.log(error);
+                console.log(error)
             }
         }
-    };
+    }
 
     const removeTask = async (index, id )=> {
-        const newTasks = [...tasks];
-        newTasks.splice(index, 1);
-        setTasks(newTasks);
+        const newTasks = [...tasks]
+        newTasks.splice(index, 1)
+        setTasks(newTasks)
 
         try {
             const deleted = await fetch(`${process.env.NEXT_PUBLIC_API_URI}/api/todos/${id}`, {
                 method: "Delete"
-            });
+            })
 
         } catch (error) {
             console.log(error)
         }
-    };
+    }
 
     return (
         <>
@@ -146,14 +159,14 @@ function Todo({ notes }) {
             </div>
         </div>
         </>
-    );
+    )
 }
 
 Todo.getInitialProps = async () => {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URI}/api/todos`);
-    const { data } = await res.json();
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URI}/api/todos`)
+    const { data } = await res.json()
   
     return { notes: data }
   }
 
-export default Todo;
+export default Todo
